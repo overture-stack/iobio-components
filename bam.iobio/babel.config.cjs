c19739ce24ca1e146c5cb62ea4848a73fc77b4bc
Dan DeMaria <ddemaria@oicr.on.ca>
2024-05-30 10:41:38 -0400

    Use Arranger Babel Config

--- bam.iobio/babel.config.cjs
+++ bam.iobio/babel.config.cjs
@@ -1,4 +1,36 @@
 module.exports = {
-	plugins: [],
-	presets: ['@babel/preset-typescript'],
+	presets: [
+		'@babel/preset-typescript',
+		'@babel/preset-env',
+		'@babel/flow',
+		[
+			'@babel/preset-react',
+			{
+				development: process.env.BABEL_ENV === 'development',
+				runtime: 'automatic',
+				importSource: '@emotion/react',
+			},
+		],
+	],
+	plugins: [
+		[
+			'module-resolver',
+			{
+				alias: {
+					'^@/public': './public',
+					'^@/(.+)': './src/\\1',
+				},
+				cwd: 'packagejson',
+				extensions: ['.js', '.jsx', '.d.ts', '.ts', '.tsx'],
+				root: ['./src'],
+			},
+		],
+		'@emotion/babel-plugin',
+		'@babel/plugin-proposal-export-namespace-from',
+		'@babel/plugin-proposal-optional-chaining',
+		'@babel/plugin-proposal-object-rest-spread',
+		'@babel/plugin-proposal-class-properties',
+		'@babel/plugin-transform-runtime',
+	],
+	sourceMaps: 'inline',
 };
